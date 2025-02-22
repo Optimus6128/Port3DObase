ead78800dda071dc4aef198dc6f397a5901c01e6
BugoTheCat <optimus6128@yahoo.gr>
2025-02-22 13:56:55 +0000

    Stupid comment for stupid situation. I mean it makes sense. There is name conflict. But why not happening with all the others?

--- src/project/experiments/spriteTests/main3DO.c
+++ src/project/experiments/spriteTests/main3DO.c
@@ -3,6 +3,12 @@
 
 #include "main3DO.h"
 
+// For some reason, those 3 defines below were in the header file like all the other experiment main3DO
+// But of I move this one it has linker issues and that's normal as they use the same name (but why doesn't it happen with all the rest?)
+// Anyway that's only because I was pressing F7 and building everything at once. If you do Ctrl+B after you set one of the projects as StartUp, it only builds that and it's ok.
+// Which means I could move them back in their header, but I will be confused and press F7 and lose half of my hour, but also in the original 3DO they are in the header so I am not changing it for all the others.
+// I want to keep the code as faithful as the original and maybe I should move those also to the header and just remember to press Ctrl+B and not just try to build all at once but only the selected project.
+
 void(*effectInitFunc[EFFECTS_NUM])() = { effectSpritesGeckoInit, effectLayersInit, effectParallaxInit, effectJuliaInit, effectWaterInit, effectSphereInit, effectFliAnimTestInit, effectAmvInit };
 void(*effectRunFunc[EFFECTS_NUM])() = { effectSpritesGeckoRun, effectLayersRun, effectParallaxRun, effectJuliaRun, effectWaterRun, effectSphereRun, effectFliAnimTestRun, effectAmvRun };
 char* effectName[EFFECTS_NUM] = { "1920 gecko sprites", "background layers", "parallax tests", "julia fractal", "water ripples", "sphere mapping", "fli animation test", "AMV bits" };
